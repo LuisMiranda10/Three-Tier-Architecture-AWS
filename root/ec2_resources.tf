a3a83f8dfdb4adefbb66ebccc6e0601c05762e61
LuisMiranda10 <luiseduardocarneiro6@gmail.com>
2024-08-28 00:00:53 -0300

    creating CloudFront and also improving some resources

--- root/ec2_resources.tf
+++ root/ec2_resources.tf
@@ -8,49 +8,49 @@ resource "aws_autoscaling_group" "three-tier-web-asg" {
 }
 
 resource "aws_autoscaling_group" "three-tier-app-asg" {
-    name = "three-tier-app-asg"
-    launch_configuration = aws_launch_configuration.three-tier-app-config.id
-    vpc_zone_identifier  = [module.network.private_subnet_ids[0].id, module.network.private_subnet_ids[1].id]
-    min_size             = 2
-    max_size             = 3
-    desired_capacity     = 2
+  name                 = "three-tier-app-asg"
+  launch_configuration = aws_launch_configuration.three-tier-app-config.id
+  vpc_zone_identifier  = [module.network.private_subnet_ids[0].id, module.network.private_subnet_ids[1].id]
+  min_size             = 2
+  max_size             = 3
+  desired_capacity     = 2
 }
 
 resource "aws_launch_configuration" "three-tier-web-config" {
-    name_prefix                 = "three-tier-web-config"
-    image_id                    = "ami-0b3a4110c36b9a5f0"
-    instance_type               = "t2.micro"
-    key_name                    = "three-tier-web-asg-kp"
-    security_groups = [aws_security_group.three-tier-web-ec2-asg-sg.id]
-    user_data =  <<-EOF
+  name_prefix                 = "three-tier-web-config"
+  image_id                    = "ami-0b3a4110c36b9a5f0"
+  instance_type               = "t2.micro"
+  key_name                    = "three-tier-web-asg-kp"
+  security_groups             = [aws_security_group.three-tier-web-ec2-asg-sg.id]
+  user_data                   = <<-EOF
         yum update -y
         yum install -y httpd
         systemctl start httpd
         systemctl enable httpd
         echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
         EOF
-    associate_public_ip_address = true
-    lifecycle {
-        prevent_destroy = true
-        ignore_changes = all
-    }
+  associate_public_ip_address = true
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = all
+  }
 }
 
 resource "aws_launch_configuration" "three-tier-app-config" {
-    name_prefix                 = "three-tier-app-config"
-    image_id                    = "ami-0b3a4110c36b9a5f0"
-    instance_type               = "t2.micro"
-    key_name                    = "three-tier-app-asg-kp"
-    security_groups = [aws_security_group.three-tier-app-ec2-asg-sg.id]
-    user_data =  <<-EOF
+  name_prefix                 = "three-tier-app-config"
+  image_id                    = "ami-0b3a4110c36b9a5f0"
+  instance_type               = "t2.micro"
+  key_name                    = "three-tier-app-asg-kp"
+  security_groups             = [aws_security_group.three-tier-app-ec2-asg-sg.id]
+  user_data                   = <<-EOF
         #!/bin/bash
 
         sudo yum install mysql -y
         EOF
-    associate_public_ip_address = false
-    lifecycle {
-        prevent_destroy = true
-        ignore_changes = all
-    }
+  associate_public_ip_address = false
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = all
+  }
 }
 
--- root/security_resources.tf
+++ root/security_resources.tf
@@ -1,93 +1,99 @@
 ## web load balancer security group
 resource "aws_security_group" "three-tier-web-sg-lb-1" {
-    name = "three-tier-web-sg"
-    description = "Allow HTTP into Load balancer security group for web tier"
-    vpc_id = module.network.vpc_id
-    depends_on = [ module.network.vpc_id ]  
+  name        = "three-tier-web-sg"
+  description = "Allow HTTP into Load balancer security group for web tier"
+  vpc_id      = module.network.vpc_id
+  depends_on  = [module.network.vpc_id]
 
-    ingress {
-        from_port   = "0"
-        to_port     = "0"
-        protocol    = "-1"
-        security_groups = ["colocar o cloudfront aqui ainda"]
-    }
-    egress {
-        from_port   = "0"
-        to_port     = "0"
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    
-    tags = {
-        Name = "three-tier-web-sg-lb-1"
+  ingress {
+    from_port       = "0"
+    to_port         = "0"
+    protocol        = "-1"
+    security_groups = ["colocar o cloudfront aqui ainda"]
+  }
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "three-tier-web-sg-lb-1"
   }
 }
 
 ## app load balancer security group
 resource "aws_security_group" "three-tier-app-sg-lb-2" {
-    name = "three-tier-app-sg-lb-2"
-    description = "Allow traffic into Load balancer security group for app tier"
-    vpc_id = module.network.vpc_id
-    depends_on = [ module.network.vpc_id ]
+  name        = "three-tier-app-sg-lb-2"
+  description = "Allow traffic into Load balancer security group for app tier"
+  vpc_id      = module.network.vpc_id
+  depends_on  = [module.network.vpc_id]
 
-    ingress {
-        from_port   = "80"
-        to_port     = "80"
-        protocol    = "tcp"
-        security_groups = [aws_security_group.three-tier-web-ec2-asg-sg.id]
-    }
-    
-    tags = {
-        Name = "three-tier-app-sg-lb-2"
-    }
+  ingress {
+    from_port       = "80"
+    to_port         = "80"
+    protocol        = "tcp"
+    security_groups = [aws_security_group.three-tier-web-ec2-asg-sg.id]
+  }
+
+  tags = {
+    Name = "three-tier-app-sg-lb-2"
+  }
 }
 
 ## web ec2 auto scaling group
 resource "aws_security_group" "three-tier-web-ec2-asg-sg" {
-    name = "three-tier-web-ec2-asg-sg"
-    description = "Allow traffic from ALB"
-    vpc_id = module.network.vpc_id
-    depends_on = [ module.network.vpc_id ]
+  name        = "three-tier-web-ec2-asg-sg"
+  description = "Allow traffic from ALB"
+  vpc_id      = module.network.vpc_id
+  depends_on  = [module.network.vpc_id]
 
-    ingress {
-        from_port   = "80"
-        to_port     = "80"
-        protocol    = "tcp"
-        security_groups = [aws_security_group.three-tier-web-sg-lb-1.id]
-    }
-    egress {
-        from_port   = "0"
-        to_port     = "0"
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    
-    tags = {
-        Name = "three-tier-web-ec2-asg-sg"
+  ingress {
+    from_port       = "80"
+    to_port         = "80"
+    protocol        = "tcp"
+    security_groups = [aws_security_group.three-tier-web-sg-lb-1.id]
+  }
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "three-tier-web-ec2-asg-sg"
   }
 }
 
 ## app ec2 auto scaling group
 resource "aws_security_group" "three-tier-app-ec2-asg-sg" {
-    name = "three-tier-app-ec2-asg-sg"
-    description = "Allow traffic from ALB"
-    vpc_id = module.network.vpc_id
-    depends_on = [ module.network.vpc_id ]
+  name        = "three-tier-app-ec2-asg-sg"
+  description = "Allow traffic from ALB"
+  vpc_id      = module.network.vpc_id
+  depends_on  = [module.network.vpc_id]
 
-    ingress {
-        from_port   = "80"
-        to_port     = "80"
-        protocol    = "tcp"
-        security_groups = [aws_security_group.three-tier-app-sg-lb-2.id]
-    }
-    egress {
-        from_port   = "0"
-        to_port     = "0"
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port       = "80"
+    to_port         = "80"
+    protocol        = "tcp"
+    security_groups = [aws_security_group.three-tier-app-sg-lb-2.id]
+  }
+  ingress {
+    from_port       = "-1"
+    to_port         = "-1"
+    protocol        = "icmp"
+    security_groups = [aws_security_group.three-tier-web-ec2-asg-sg]
+  }
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "three-tier-app-ec2-asg-sg"
-  } 
+  tags = {
+    Name = "three-tier-app-ec2-asg-sg"
+  }
 }
